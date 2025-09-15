1d2c192a9b9c975ec6e9ccebc9574f712e7bdb41
Ali_Mohamed <148333528+aliabubeah@users.noreply.github.com>
2025-09-15 19:50:08 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -6,13 +6,5 @@ const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
     <React.StrictMode>
         <App />
-        {/* <StartRating maxRating={5} />
-        <StartRating
-            maxRating={5}
-            color="blue"
-            size={30}
-            defaultRating={4}
-            messages={["bad", "okay", "good", "verygood", "amazing"]}
-        /> */}
     </React.StrictMode>
 );
